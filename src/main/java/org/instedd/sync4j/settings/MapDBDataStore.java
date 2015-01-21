6fe5f20aee860d4c16f30b7b1aab220d8b5ad0f0
Franco Leonardo Bulgarelli <fbulgarelli@manas.com.ar>
2015-01-21 12:05:35 -0300

    Adding MapDBDataStore

---
+++ src/main/java/org/instedd/sync4j/settings/MapDBDataStore.java
@@ -0,0 +1,42 @@
+package org.instedd.sync4j.settings;
+
+import java.io.File;
+import java.util.NavigableMap;
+
+import org.instedd.sync4j.Settings;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+
+class MapDBDataStore extends StringsSettingsStore {
+
+  private NavigableMap<String, String> treeMap;
+  private DB db;
+
+  public MapDBDataStore(DB db, NavigableMap<String, String> treeMap) {
+    this.db = db;
+    this.treeMap = treeMap;
+  }
+
+  @Override
+  public void setSettings(Settings settings) {
+    super.setSettings(settings);
+    db.commit();
+  }
+
+  protected String get(String key, String defaultValue) {
+    return treeMap.get(key);
+  }
+
+  protected void set(String key, String value) {
+    if(value != null) {
+      treeMap.put(key, value);
+    }
+  }
+
+  public static MapDBDataStore fromMapDB(String databaseFilename) {
+    DB db = DBMaker.newFileDB(new File(databaseFilename)).closeOnJvmShutdown().make();
+    NavigableMap<String, String> map = db.getTreeMap("settings");
+    return new MapDBDataStore(db, map);
+  }
+
+}
